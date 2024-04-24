18b960462123592b7c6d067b16382d2207b5c383
Richelieu <richelieu042@gmail.com>
2024-04-24 15:24:41 +0800

    init

--- src/_chimera/jwt/JWTUtil.ts
+++ src/_chimera/jwt/JWTUtil.ts
@@ -1,11 +1,22 @@
-import {sign} from "jsonwebtoken";
+/*
+ * jose: https://www.npmjs.com/package/jose
+ * jsonwebtoken库不能用（貌似需要nodejs环境）.
+ */
+import * as jose from 'jose';
 
 export class JWTUtil {
     /*
-     * 默认: HS256
+     * 参考: https://github.com/panva/jose/blob/HEAD/docs/classes/jwt_sign.SignJWT.md
      */
-    static sign(payload: any, secret: string): string {
-        return sign(payload, secret);
-    }
+    static sign(payload: any, alg: string = "HS256", secret: string) {
+        const secretUA = new TextEncoder().encode(secret)
 
+        return new jose.SignJWT({'urn:example:claim': true})
+            .setProtectedHeader({alg})
+            .setIssuedAt()
+            .setIssuer('urn:example:issuer')
+            .setAudience('urn:example:audience')
+            .setExpirationTime('2h')
+            .sign(secretUA);
+    }
 }
