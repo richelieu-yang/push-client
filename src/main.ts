0cc9bcdd91bf0533b8c7648797fa1126b1bbbac3
Richelieu <richelieu042@gmail.com>
2024-04-29 14:29:10 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import './assets/main.css';
 
 import {createApp} from 'vue';
 import App from './App.vue';
+import {UuidKit} from "@/_chimera/id/UuidKit";
 
 createApp(App).mount('#app');
+
+console.log(UuidKit.v4());
+console.log(UuidKit.simpleV4());
