088496c588ad94aef89d1d155e2d8e8eade408ad
Richelieu <richelieu042@gmail.com>
2024-04-29 13:28:58 +0800

    init

--- src/_chimera/jwt/JwtKit.ts
+++ src/_chimera/jwt/JwtKit.ts
@@ -8,15 +8,21 @@ export class JwtKit {
     /*
      * 参考: https://github.com/panva/jose/blob/HEAD/docs/classes/jwt_sign.SignJWT.md
      */
-    static sign(payload: any, alg: string = "HS256", secret: string) {
-        const secretUA = new TextEncoder().encode(secret)
+    static sign(payload: any, alg: string = "HS256", secret: string, expirationTime: string) {
+        if (!payload) {
+            payload = {};
+        }
 
+        const secretUA = new TextEncoder().encode(secret)
         return new jose.SignJWT(payload)
-            .setProtectedHeader({alg})
+            .setProtectedHeader({
+                alg: alg,
+                typ: "JWT",
+            })
             .setIssuedAt()
             // .setIssuer('urn:example:issuer')
             // .setAudience('urn:example:audience')
-            // .setExpirationTime('2h')
+            .setExpirationTime(expirationTime)
             .sign(secretUA);
 
         //     return new jose.SignJWT({'urn:example:claim': true})
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import './assets/main.css';
 
 import {createApp} from 'vue';
 import App from './App.vue';
+import {JwtUtil} from "@/_internal/utils/JwtUtil";
 
 createApp(App).mount('#app');
+
+let str = await JwtUtil.genSubToken(null, "HS256", "c27022ed-f258-46ca-b894-6d54e5db4ce7", "2h", "1073", "example");
+console.log(str);
